2e13923e602639b5563a47de07b526c542f399eb
Michael Kelly <mike@whatsthatweb.com.au>
2017-03-20 19:15:57 +1100

    Converting to using pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -127,6 +127,15 @@ gulp.task('scripts', () =>
       .pipe(gulp.dest('.tmp/scripts'))
 );
 
+// scan the views directory for pug files
+gulp.task('views', () =>
+  gulp.src('views/**/*.pug')
+  .pipe($.pug())
+  // .pipe(gulp.dest('app'))
+  .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest('.tmp'))
+);
+
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
   return gulp.src('app/**/*.html')
@@ -156,7 +165,7 @@ gulp.task('html', () => {
 gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
 // Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['scripts', 'styles', 'views'], () => {
   browserSync({
     notify: false,
     // Customize the Browsersync console logging prefix
@@ -174,6 +183,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
   gulp.watch(['app/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
+  gulp.watch(['views/**/*.pug'], ['views', reload]);
   gulp.watch(['app/images/**/*'], reload);
 });
 
@@ -197,7 +207,7 @@ gulp.task('serve:dist', ['default'], () =>
 gulp.task('default', ['clean'], cb =>
   runSequence(
     'styles',
-    ['lint', 'html', 'scripts', 'images', 'copy'],
+    ['lint', 'views', 'html', 'scripts', 'images', 'copy'],
     'generate-service-worker',
     cb
   )
